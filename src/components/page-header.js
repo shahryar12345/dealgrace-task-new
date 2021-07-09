e08d9c2d8491f86ab21734183bfd709ff86ce34f
Shahryar Shahid <shehriyarshahid162@gmail.com>
2021-07-10 02:48:15 +0500

    Add search icon in page header.

--- src/components/page-header.js
+++ src/components/page-header.js
@@ -1,5 +1,7 @@
 import logo from "../images/dealgrace_logo.png";
-import menuIcon from "../images/hamburger-icon.svg"
+import menuIcon from "../images/hamburger-icon.svg";
+import searchIcon from "../images/search-icon.svg";
+
 import React from "react";
 
 const PageHeader = () => {
@@ -8,7 +10,7 @@ const PageHeader = () => {
    <div className={"logo-menu-container"}>
     <div className={"icon-Container"}>
      <a href="?">
-      <img alt="logo-" src={logo} className="logo"></img>
+      <img alt="logo-company" src={logo} className="logo"></img>
      </a>
     </div>
     <div className={"menu-login-Container"}>
@@ -16,8 +18,12 @@ const PageHeader = () => {
       Login
      </a>
     </div>
+    <div className={"search-Icon-Container"}>
+     <img alt="logo-search-icon" src={searchIcon} className="search-Icon"></img>
+    </div>
+
     <div className={"menu-icon-container"}>
-    <img alt="logo-" src={menuIcon} className="menu-icon"></img>
+     <img alt="logo-menu" src={menuIcon} className="menu-icon"></img>
     </div>
    </div>
   </div>
