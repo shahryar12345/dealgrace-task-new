1ba4eec2c60fcb3220a118c6eac6b53ca15613af
Shahryar Shahid <shehriyarshahid162@gmail.com>
2021-08-07 02:16:19 +0500

    add some extras

---
+++ others-Extras/deal-grace-test/src/App.js
@@ -0,0 +1,71 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+ return (
+  <div className="App">
+   {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+   <form>
+    {/* <h1>Hello</h1>
+    <p>Enter your name:</p>
+    <input type="text" name="txtFname" placeholder="First Name" required />
+    <label class="control-label" for="username">
+     Username
+    </label>{" "}
+    <span class="required">*</span>
+    <input type="text" id="username1" name="data[username]" required="required" class="form-control"></input>
+
+    <input type="text" id="username1" name="data[username]" required="required" class="form-control"></input>
+    
+    <input type="text" id="username2" name="data[username]"  required="required" class="form-control"></input> */}
+
+
+    {/* <label class="control-label required" for="username">Username</label> */}
+    {/* <input type="text" id="username2" name="data[username]"  required="required" class="form-control">    </input> */}
+    {/* <input type="text" id="username2" name="data[username]"  required="required" class="form-control">    </input> */}
+
+    {/* <input type="text" id="username2" name="data[username]"  required="required" class="form-control">  </input> */}
+
+{/* <span class="fa fa-info-circle errspan"></span> */}
+{/* <input name="txtName" id="txtName"></input>
+
+<span class="fa fa-info-circle errspan"></span>
+
+
+
+<div class="input-wrapper">
+     <input type="text" />
+ </div> */}
+
+{/* 
+ <div class="input-wrapper">
+  <input id="stuff"/>
+  <label for="stuff" class="fa fa-user input-icon"></label>
+</div> */}
+
+
+<h1 style="background-color:DodgerBlue;">Hello World</h1>
+<p style="background-color:Tomato;">Lorem ipsum...</p>
+
+   </form>
+  </div>
+
+
+
+ );
+}
+
+export default App;
